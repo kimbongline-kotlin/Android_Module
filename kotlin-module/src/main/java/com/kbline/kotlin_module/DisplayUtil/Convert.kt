33053ed78adc944ca142313283af440149058232
kimbongline-kotlin <qhdtjdcks@naver.com>
2020-02-19 00:49:05 +0900

    modify

--- kotlin-module/src/main/java/com/kbline/kotlin_module/DisplayUtil/Convert.kt
+++ kotlin-module/src/main/java/com/kbline/kotlin_module/DisplayUtil/Convert.kt
@@ -3,7 +3,10 @@ package com.kbline.kotlin_module.DisplayUtil
 import android.content.Context
 import android.util.DisplayMetrics
 
+//DP to Pixcel, Pixcel to DP 변환
 object Convert {
+
+
 
     fun toPixcel(dp: Float, context: Context): Float {
         return context.resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT * dp
